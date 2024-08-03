72574a00e10094570996a8a8b800146e2de9de4a
DELL <pallavipradhan5578@gmail.com>
2024-08-03 14:58:42 +0530

    initial commit

---
+++ add_program.dart
@@ -0,0 +1,11 @@
+void main(){
+  int num1;
+  int num2;
+  int result;
+  num1 = 76;
+  num2 = 67;
+  result = num1 - num2;
+    print(num1);
+  print(num2);
+  print(result);
+}
---
+++ arithmetic opreation.dart
@@ -0,0 +1,21 @@
+void main(){
+  int num1;
+  int num2;
+  int resultSum;
+  int resultSubstration;
+  int resultMultiple;
+  int resultDivide;
+  num1 = 6;
+  num2 = 8;
+  resultSum = num1  + num2;
+  resultSubstration = num1 - num2;
+  resultMultiple = num1  * num2;
+  resultDivide = num1 |num2;
+  print("$num1 \n $num2");
+  print(resultSum);
+  print(resultSubstration);
+  print(resultMultiple);
+  print(resultDivide);
+    
+
+}

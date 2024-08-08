dcfecafd64fc0918075c1a9f252ce92bba4dc4c8
DELL <pallavipradhan5578@gmail.com>
2024-08-08 17:33:34 +0530

    Increment & Decrement operator program.dart

---
+++ lib/ Increment & Decrement oprator prg.dart
@@ -0,0 +1,23 @@
+void main()
+{
+  int x =10;
+   /*x++;//postfix increment means(x+1== x++)
+   print(x);
+  x--;//postfix decrement means(x-1 == x--)
+  print (x);
+  x= 100;
+  ++x; 
+  print(x);//prefix increment
+  --x;   // 
+  print(x);//prefix decrement*/
+  print(++x);//increment fir written what's why print 11 
+  print(--x); //decrement fir written what's why print 10 
+  int y = 100;
+
+  print(y++);//written fir increment what's why print 1oo not 101
+  print(y--);//written fir increment what's why print 1o1 not 100
+  
+
+
+
+}

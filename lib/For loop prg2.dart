c67abef1058168203bd57c1f3543e823a92e34ca
DELL <pallavipradhan5578@gmail.com>
2024-08-09 17:28:35 +0530

    For loop prg3.dart

---
+++ lib/For loop prg2.dart
@@ -0,0 +1,26 @@
+void main(){
+  for(int i= 1;i<=10;i++){
+ if ( i % 2==0){
+  print ("even number are $i ");
+  }
+  else{
+    print ("odd numbers are $i");
+  }
+  }
+   /*if (i == 5){
+      print ("value checked");
+     break;  // break is a keyword
+      //continue;
+    }else { 
+      print (i);*
+    }
+     print ("value of $i and sum ${i+i} ");
+      }
+      print("end");
+    }
+  }
+ } 
+*/
+
+  
+  }

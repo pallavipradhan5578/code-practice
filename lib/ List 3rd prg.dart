5bc352a6342b31232537be0f4fa903176abfd8f4
DELL <pallavipradhan5578@gmail.com>
2024-08-10 18:05:50 +0530

    List 3rd prg. dart

---
+++ lib/ List 3rd prg.dart
@@ -0,0 +1,7 @@
+void main(){
+  List<String>name = ["Shivya","Pallavi","Jyoti","Anandita","Tara"];
+  print(name.length);
+  for(int i =0;i<name.length;i++){
+    print("name is ${name[i]}");
+  }
+}

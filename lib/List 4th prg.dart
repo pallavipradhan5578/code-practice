a3408ffdda3f0e7e7be86540d22624eca140f792
DELL <pallavipradhan5578@gmail.com>
2024-08-10 19:09:16 +0530

    List 4th prg.dart

--- lib/List 4th prg.dart
+++ lib/List 4th prg.dart
@@ -1,4 +1,11 @@
 void main(){
-  List<int>age = [23,24,25,26,27,28,29,30,31];
-  List<age>
-}
+  List<int>age = [23,24,25];
+  List<String>name = ["paro","pari","sofi"];
+  List<bool>boolList = [true, false,true];
+  //print(name.length);
+  
+  for( int i = 0; i<name.length;i++){
+  
+    print(
+      "My name is ${name[i]} and age is ${age[i]} and my lucky${boolList[i]}");}
+  }

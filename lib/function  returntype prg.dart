30a836a0bda124cd5d4820532418d4c66ecf3caa
DELL <pallavipradhan5578@gmail.com>
2024-08-14 16:14:14 +0530

    Function return type prg.dart

---
+++ lib/function  returntype prg.dart
@@ -0,0 +1,14 @@
+void main(){
+sum();
+print(name());
+print(substract(90,30));
+}
+
+ void sum( ){
+  int x =10,y=30;
+  print("x+y"); }
+  String name(){
+  return  "pallavi kumari";}
+  int substract(int x , int y,){
+  return x-y;
+  }

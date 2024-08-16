07d10a34c6c9356dbd3096c420a7171790df58f8
DELL <pallavipradhan5578@gmail.com>
2024-08-16 18:21:09 +0530

    Switch statement 2nd prg.dart

---
+++ lib/Switch statement 2nd prg.dart
@@ -0,0 +1,15 @@
+void main(){
+int x= 100,y =40;
+ var operation = '-';
+ if (operation == '-'){
+  print(y-x);
+ }
+ switch (operation){
+  case "+":
+  print(x+y);
+  return;
+  case "-":
+  print(x-y);
+  return;
+ }
+}

28cae8667af56d653622219ad8a4410aa96b0cb5
DELL <pallavipradhan5578@gmail.com>
2024-08-17 10:02:31 +0530

    Prg to print weekdaysname.dart

--- lib/Hash map 2nd prg.dart
+++
@@ -1,5 +0,0 @@
-void main(){
-  Map<String ,dynamic>map= {"name": "pallavi",'age':40};
-  Map <String,int>map1= {"Sona": 1};
-  print(map1);
-}
---
+++ lib/prg to print weekdays.dart
@@ -0,0 +1,6 @@
+void main(){
+  List<String> daysName=["Sonday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+  List<int> index=[1,2,3,4,5,6,7];
+  for (int i=0;i<index.length;i++){
+  print("Weekday name are ${daysName[i]}");
+}}

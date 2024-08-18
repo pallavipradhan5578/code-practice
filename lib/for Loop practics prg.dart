c9b61cf2c297fe5a57dfcd3ca76e15bf496d8500
DELL <pallavipradhan5578@gmail.com>
2024-08-18 13:07:48 +0530

    For loop practics prg.dart

---
+++ lib/for Loop practics prg.dart
@@ -0,0 +1,15 @@
+void main(){
+name1();
+name2();}
+void name1(){
+  for(int i =1;i<=10;i++){print ("pallavi");}
+  print("Kaushiki kumari");
+  return;
+}
+name2(){
+  for(int i = 1;i<=10;i++){
+    if(i==5){
+      print("Pradhan");
+    }
+    print(i);}
+}

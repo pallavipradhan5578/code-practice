f801482cc9bb34b81f6cdb0f388492f3f03642b2
DELL <pallavipradhan5578@gmail.com>
2024-08-19 22:41:27 +0530

    function prg1.dart

--- lib/Function return type 2ndprg.dart
+++ lib/Function return type 2ndprg.dart
@@ -1,7 +1,19 @@
 void main(){
   print(findmaximumnumber());
 }
-int findmaximumnumber(){
+int void main(){
+ add(20,30); 
+
+}add(int a,int b){
+  int c =a+b;
+  print(c);
+
+  int d= c+a+b;
+  print(d);
+  int e=c-a+b;
+  return e;
+
+}findmaximumnumber(){
   List<int>numberList=[23,24,25,567,3434,-33];
   int max =0;
   max= numberList[0];
---
+++ lib/function prg1.dart
@@ -0,0 +1,13 @@
+void main(){
+ add(20,30); 
+
+}add(int a,int b){
+  int c =a+b;
+  print(c);
+
+  int d= c+a+b;
+  print(d);
+  int e=c-a+b;
+  return e;
+
+}

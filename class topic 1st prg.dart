36730c396ba0c265afc00cf4208448c12401b774
DELL <pallavipradhan5578@gmail.com>
2024-08-20 16:38:02 +0530

    class topic 1st prg.dart

---
+++ class topic 1st prg.dart
@@ -0,0 +1,15 @@
+void main(){
+Person person=Person();
+person.displayInfo();
+}
+class Person{
+  String name = "Pallavi";
+  String  fatherName ="Ramsubhash Pradhan";
+  int age= 22;
+  void displayInfo(){
+  print("My name is ${name}");
+  print("My father name is ${fatherName}");
+  print("My age is ${age}");
+
+  }
+}

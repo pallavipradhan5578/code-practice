5a0d0afa94d9c5925a895fce4490dd4a631652dd
DELL <pallavipradhan5578@gmail.com>
2024-08-23 14:38:37 +0530

    Constructor 4th prg.dart

--- lib/Constructor 4th prg.dart
+++ lib/Constructor 4th prg.dart
@@ -1,16 +1,14 @@
 void main(){ 
   Student student=Student("Atif Taj");
-  
+  //student.name=("Asif");
   student.displayInfo();
 
 }class Student{
   String? name; int? age;
-  Student(String name){// Constructor/r
+  Student(String name){
     print("called");
-     name= name;
-    print(name);
-
-    }
+    name=name;
+    print(name);}
     void displayInfo(){
       print("----------------------");
       print("Name : $name");

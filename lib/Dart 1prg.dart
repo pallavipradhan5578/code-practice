ce95fd503a1a713ecfa23a917ffd99ef5eefc7a0
DELL <pallavipradhan5578@gmail.com>
2024-08-28 11:12:55 +0530

    1st dart prg .dadr

--- lib/Dart 1prg.dart
+++ lib/Dart 1prg.dart
@@ -9,7 +9,7 @@
   print("*                    *");
   print("**********************");print('Family name:-');
   print('1. Ramsubhash Pradhan');print("2. Anandita Kumari Rai");
-  print("3. Jyoti Kumari");print('4. Nikhilesh Pradhan');print('5. Pallavi');
+  print(". Jyoti Kumari");print('4. Nikhilesh Pradhan');print('5. Pallavi');
 
   
   

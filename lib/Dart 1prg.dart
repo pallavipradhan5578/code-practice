de7c27b0c00d0102be6e4b16218e1b3024c13d3c
DELL <pallavipradhan5578@gmail.com>
2024-08-28 13:45:03 +0530

    dart 1st prg .dart

--- lib/Dart 1prg.dart
+++ lib/Dart 1prg.dart
@@ -10,6 +10,31 @@
   print("**********************");print('Family name:-');
   print('1. Ramsubhash Pradhan');print("2. Anandita Kumari Rai");
   print(". Jyoti Kumari");print('4. Nikhilesh Pradhan');print('5. Pallavi');
+  //single Line comments methods
+  //this is my first prg.dartprint("Hello World");
+  print('Square shape:-');
+  print("**********************");print("*                    *");                   
+  print("*                    *");
+  print("*                    *");
+  print("*                    *");
+  print("*                    *");
+  print("**********************");
+  //multi line comment methods:-
+  /*
+   In this file:- 
+   1. we print Hello world and square shape
+   2.also print family name
+  */
+  print('Family name:-');
+  print('1. Ramsubhash Pradhan');print("2. Anandita Kumari Rai");
+  print("3. Jyoti Kumari");print('4. Nikhilesh Pradhan');print('5. Pallavi');
+  
+  // method for terminate line by using \n
+  print("My name is pallavi.\n I am from bihar");
+  print("    #   \n   ###    \n  #####")    ;
+
+
+
 
   
   

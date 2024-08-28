8d66f1d41f2cf82fec2d47f45245378550ae3684
DELL <pallavipradhan5578@gmail.com>
2024-08-28 11:03:25 +0530

    Dart 1st prg.dart

---
+++ lib/Dart 1prg.dart
@@ -0,0 +1,18 @@
+  
+  void main(){
+  print("Hello World");
+  print('Square shape:-');
+  print("**********************");print("*                    *");                   
+  print("*                    *");
+  print("*                    *");
+  print("*                    *");
+  print("*                    *");
+  print("**********************");print('Family name:-');
+  print('1. Ramsubhash Pradhan');print("2. Anandita Kumari Rai");
+  print("3. Jyoti Kumari");print('4. Nikhilesh Pradhan');print('5. Pallavi');
+
+  
+  
+  
+
+}

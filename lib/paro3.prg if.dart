44c9dad619fcd3511202e6554a26c254cb426f59
DELL <pallavipradhan5578@gmail.com>
2024-08-29 21:18:15 +0530

    check vovel or consonant prg.dart

--- lib/paro3.prg if.dart
+++ lib/paro3.prg if.dart
@@ -1,10 +1,30 @@
 void main(){
-  int x=-67,y=89, z=12;
+  /*int x=-67,y=89, z=12;
    
   if(x<y){
     print("  $y this is greater than");
   }else if(x>y){print("$x this  is greater");}
   else if  (y<z){
-    print(" $z this  is greater");
-  }
-}
+    print(" $z this  is greater");}*/
+          
+    String vowel="a,e,i,o,u";
+     if(vowel=="a,e,i,o,u"){
+      /*print("A is vowel ");}else if
+      (vowel=="e"){
+        print("E is vowel ");}else if 
+        (vowel =="i"){
+          print("I is vowel");}else if
+          (vowel=="o"){
+            print("  O is vowel");}else if 
+            (vowel=='u'){
+              print("U is Also vowel" );}
+              else{*/
+               print(" A,E,I,O,U  are vowel");
+               }else{
+                print("A,E,I,O,U are consonant");
+               }
+               }
+
+
+
+     

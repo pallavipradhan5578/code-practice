ea85b000dff24fda44d3f49c147d040929ea7d81
DELL <pallavipradhan5578@gmail.com>
2024-08-29 19:18:14 +0530

    2nd if practics prg.dart

--- lib/paro3.prg if.dart
+++ lib/paro3.prg if.dart
@@ -1,9 +1,10 @@
 void main(){
-  int x=-67;
-  if(x<0){
-    print("$x is negative  number");
-  }else if(x>0){print("$x is positive number");}
-  else{
-    print("$x is zero");
+  int x=-67,y=89, z=12;
+   
+  if(x<y){
+    print("  $y this is greater than");
+  }else if(x>y){print("$x this  is greater");}
+  else if  (y<z){
+    print(" $z this  is greater");
   }
 }

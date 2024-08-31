ecf884fed2997da35c3f3a6a18e0c83e77b3d514
DELL <pallavipradhan5578@gmail.com>
2024-08-31 13:49:39 +0530

    Calculate the sum of the first 50 natural numbers using a 'for' loop.dart

--- lib/for Ioop prg print num 1-10.dart
+++ lib/for Ioop prg print num 1-10.dart
@@ -1,5 +1,6 @@
 void main(){
-  
-  for(int i=1;i<10;i++){
-  print("Hello ${i+1}");}
+  int sum=0;
+  for(int i=1;i<=50;i++){
+    sum+=i;
+  print("The sum of the first 50 natural numbers is : $sum");}
 }

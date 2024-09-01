978c54ce0869a9a32528071d1fb1bcfc69ca1b25
DELL <pallavipradhan5578@gmail.com>
2024-09-01 14:16:56 +0530

    1 sep prg2.dart

--- lib/1sep prg  .dart
+++ lib/1sep prg  .dart
@@ -2,6 +2,11 @@ void main(){
   String  name="pallavi";
   String fatherName ="Ramsubhash";
   String MotherName="Anandita";
+  int age =19;
   print("My name is $name . My fathername is $fatherName and my motherName is $MotherName. ");
-
+if(age>=18){
+  print("$name you are eligible for voting");
+} else{
+  print("$name  you are not eligible for voting");
+}
 }

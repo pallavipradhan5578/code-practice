1e7700159d9518c895f10835f4d4ef474bcd1922
DELL <pallavipradhan5578@gmail.com>
2024-09-04 13:59:54 +0530

    celsius to fohrenheit prg .dart

---
+++ lib/variable and data types prg.dart
@@ -0,0 +1,14 @@
+void main(){
+  String name="pallavi pradhan";
+  (" Name : $name");
+  double  celsius= 37.0;
+  double fohrenheit=(celsius*9/5) +32;
+  print("$celsius  degree celsius is eual to $fohrenheit");
+  int age=22;
+  double height=5.2;
+  String FatherName="Taj Gul",studentName='Asif Taj';
+  var motherName="Nur Ahmed";
+bool isStudent=true;
+print("Studentname : $studentName");
+print("Fathername : $FatherName\nMothername : $motherName\nHeight : $height\nStudent pass : $isStudent \nAge : $age");
+}

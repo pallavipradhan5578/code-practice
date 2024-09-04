05d9ef1c93cd9bd07ada40105a32e3b0da7a271e
DELL <pallavipradhan5578@gmail.com>
2024-09-04 11:29:25 +0530

    control flow statement prg.dart

--- lib/variable 1prg .dart
+++ lib/variable 1prg .dart
@@ -1,8 +1,20 @@
 
 void main(){
-  String name="Pallavi Pradhan";
+  //1st prg
+  /*String name="Pallavi Pradhan";
   int age=22;
   double height=5.2;
   bool isStudent=true;
-  print("Name : $name\nAge : $age \nHeight : $height\nStudent : $isStudent");
+  print("Name : $name\nAge : $age \nHeight : $height\nStudent : $isStudent");*/
+  // 2nd prg
+  int score=85;
+  if(score>=90){
+    print("Grade : A");
+
+  }else if(score>=80){
+    print("Grade : B");
+
+  }else{
+    print("Grade : C");
+  }
 }

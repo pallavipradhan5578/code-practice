96d65db2ab378fb849b539876da50003723f3f6e
DELL <pallavipradhan5578@gmail.com>
2024-09-04 11:22:38 +0530

    Variable and datatype prg.dart

---
+++ lib/variable 1prg .dart
@@ -0,0 +1,8 @@
+
+void main(){
+  String name="Pallavi Pradhan";
+  int age=22;
+  double height=5.2;
+  bool isStudent=true;
+  print("Name : $name\nAge : $age \nHeight : $height\nStudent : $isStudent");
+}

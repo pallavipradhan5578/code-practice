93defb41621e5d972bd0cd314de3f328f403d5f9
DELL <pallavipradhan5578@gmail.com>
2024-09-04 12:15:29 +0530

    List prg.dart

--- lib/variable 1prg .dart
+++ lib/variable 1prg .dart
@@ -1,20 +1,30 @@
 
+import 'dart:mirrors';
+
 void main(){
-  //1st prg
+//1st prg
   /*String name="Pallavi Pradhan";
   int age=22;
   double height=5.2;
   bool isStudent=true;
   print("Name : $name\nAge : $age \nHeight : $height\nStudent : $isStudent");*/
-  // 2nd prg
-  int score=85;
+// 2nd prg
+  /*int score=85;
   if(score>=90){
     print("Grade : A");
-
-  }else if(score>=80){
+    }else if(score>=80){
     print("Grade : B");
-
-  }else{
+    }else{
     print("Grade : C");
-  }
+  }*/
+//3rd prg
+/*  int result=add(5,9);
+    print("Sum : $result");
+    }int add(int a,int b){
+    return a+b;*/
+    List<String>fruit=["Apple","Banana","Mango"];
+    fruit.add("Orange");print("These are my favourite fruits:$fruit");
+    print(fruit.length);
+    print(fruit[2]);
+
 }

609b2c8d76aea720535301a9a75bb2fe0a06e0d6
DELL <pallavipradhan5578@gmail.com>
2024-09-06 13:01:58 +0530

    print num from 1 to 50 using for a loop.dart

--- lib/Loop practics prg.dart
+++ lib/Loop practics prg.dart
@@ -1,13 +1,4 @@
 void main(){
-  int x=20;
-  for(int i =1;i<= 20;i++){
-    print(i);
-  
-  if(i%2==0){
-    print ("$i is even number");
-
-  }else if(i%2!=0){
-    print("$i is odd number");
-  }
-  }
-}
+  int x=50;
+  for(int i =1;i<= 50;i++){
+    print(i);}}

ab2220501be2da2d79eb2d89f18d9f016341e4e9
DELL <pallavipradhan5578@gmail.com>
2024-09-07 15:31:10 +0530

    simple calculator prg dart

---
+++ lib/simple cal.dart
@@ -0,0 +1,8 @@
+void main(){
+  int num1=18,num2=9;
+  print("addition : ${num1+num2}");
+  print("substraction : ${num1-num2}");
+  print("multiplication : ${num1*num2}");
+  print("division : $num1/$num2" );
+  
+}

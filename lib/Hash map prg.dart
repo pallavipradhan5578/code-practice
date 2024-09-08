3f9dc95779dda6033cd78da80712100a55452ac5
DELL <pallavipradhan5578@gmail.com>
2024-09-08 12:31:32 +0530

    Map practict prg .dart

--- lib/Hash map prg.dart
+++ lib/Hash map prg.dart
@@ -4,3 +4,4 @@ void main(){
   print(map['name']);
 
 }
+
--- lib/futrue delay function prg .dart
+++ lib/futrue delay function prg .dart
@@ -1,7 +1,12 @@
-void main()async{
+
+void main(){
+ // main1();
+//}
+/*void main1()async{
   String name="pallavi";
   print(name);
   await Future.delayed(Duration(seconds: 5));
-  print("My name is $name");
+  print("My name is $name");*/
 
-}
+Map<String,dynamic> map={"Name" :"Asif Taj","Age": "22"};
+ print(map['Name']);}

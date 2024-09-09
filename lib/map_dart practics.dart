d0a7f6052c306db7ab076a1c48b29b719975b1ed
DELL <pallavipradhan5578@gmail.com>
2024-09-09 10:31:05 +0530

    Map practics prg.dart

---
+++ lib/map_dart practics.dart
@@ -0,0 +1,17 @@
+void main(){
+  var map_name={
+    'key': "pallavi",
+    'key1' : 22,
+    'key2':"bihar",
+    'key3':'true'
+    };
+    print(map_name['key1']);
+    map_name['key1']='raman';//for overright key1
+
+    
+  print(map_name['key1']);
+  map_name['Key1']='ram';//for add Key1
+  print(map_name);
+    
+
+}

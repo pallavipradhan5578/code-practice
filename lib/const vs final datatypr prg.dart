4ad050d1cfb3aaed8d7c8a5ea36438bb52439025
DELL <pallavipradhan5578@gmail.com>
2024-09-09 17:30:31 +0530

    final vs const datatype prg 2.dart

---
+++ lib/const vs final datatypr prg.dart
@@ -0,0 +1,13 @@
+
+
+void main(){
+  final names= [
+    "Raman",
+    "Siya" ,
+    "Misha",
+    "Omkar"//final datatype me compile time changes ho sakta h pr const data type me nhi
+
+  ] ;
+names.add("Yuvi");
+print(names);
+}

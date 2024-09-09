73892913fc19c1768ecadf7b27e7231a70eb251a
DELL <pallavipradhan5578@gmail.com>
2024-09-09 10:43:49 +0530

    Hash map practicsprg12.dart

--- lib/map_dart practics.dart
+++ lib/map_dart practics.dart
@@ -1,5 +1,5 @@
 void main(){
-  var map_name={
+  /*var map_name={
     'key': "pallavi",
     'key1' : 22,
     'key2':"bihar",
@@ -11,7 +11,16 @@ void main(){
     
   print(map_name['key1']);
   map_name['Key1']='ram';//for add Key1
-  print(map_name);
-    
+  print(map_name);*/
+   var map_name={
+    "name": "value1",
+    'YearOfExperience':4,
+    "Avg.rating": 3.0,
+    "CanLocateToOffice":'true'
+
+   } ;
+
+map_name["name"]="Asif TAj";
+print(map_name);
 
 }

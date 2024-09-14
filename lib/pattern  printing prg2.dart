c3074d7df4b4a70e60bb3770998c05c8a72880fe
DELL <pallavipradhan5578@gmail.com>
2024-09-14 14:28:47 +0530

    14 sep practics prg.dart

---
+++ lib/pattern  printing prg2.dart
@@ -0,0 +1,12 @@
+void main(){
+  Hello();
+  Eduationalbackground();
+}
+void Hello(){
+  print("Hello!");
+  print("My name is pallavi.");
+  print("Basically i am from bihar. ");
+}
+Eduationalbackground(){
+print("I have done my graduation with 72% from vksu Ara.");
+}
--- lib/pattern printing prg by using Loop.dart
+++ lib/pattern printing prg by using Loop.dart
@@ -10,4 +10,5 @@ void main(){
 
 print('');
   }
+  
 }
